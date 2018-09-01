a1dd98f080684dcc6bd421b9711b7e5cc66a3586
dokipen3d <danielelliott3d@gmail.com>
2018-09-02 10:45:36 +1200

    make resources all unique

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <set>
 //#define VULKAN_HPP_NO_EXCEPTIONS
-#include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan2.hpp>
 
 static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
                                                     VkDebugUtilsMessageTypeFlagsEXT messageType,
@@ -27,6 +27,7 @@ class WindowUtil {
             glfwRequiredInstanceExtensions = { glfwExtensions, glfwExtensions + glfwExtensionCount };
         }
     }
+    ~WindowUtil() { glfwTerminate(); }
 
     uint32_t getRequiredExtensionCount() { return glfwExtensionCount; }
     std::vector<const char*> getRequiredExtensions() { return glfwRequiredInstanceExtensions; }
@@ -41,42 +42,50 @@ class Window {
     Window(const vk::Instance& instance)
     : window([&]() {
           glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-          window = glfwCreateWindow(640, 480, "My Title", nullptr, nullptr);
+          // window = glfwCreateWindow(640, 480, "My Title", nullptr, nullptr);
 
           if (glfwVulkanSupported()) {
               std::cout << "Vulkan is available, at least for compute" << std::endl;
           } else {
               std::cout << "Vulkan is NOT available" << std::endl;
           }
-          glfwSetKeyCallback(window, key_callback);
 
-          return window;
-
+          return glfwCreateWindow(640, 480, "My Title", nullptr, nullptr);
       }()),
       us([&]() {
           VkSurfaceKHR surface;
-          VkResult err = glfwCreateWindowSurface(instance, window, nullptr, &surface);
+          VkResult err = glfwCreateWindowSurface(instance, window.get(), nullptr, &surface);
           if (err) {
               std::cout << "Window surface creation failed" << std::endl;
           }
-          return vk::UniqueSurfaceKHR{ surface, instance };
-      }()) {}
-    ~Window() {
-        glfwDestroyWindow(window);
-        glfwTerminate();
+          std::cout << "creating surface" << std::endl;
+          return vk::UniqueSurfaceKHR(surface, instance);
+      }()) {
+        glfwSetKeyCallback(window.get(), key_callback);
     }
+    //~Window() {} //DONT DECLARE THIS UNLESS ABSOLUTELY NECESSARY! If it is declared, then the
+    //default move constructors/assignments are deleted and will need to be default declared
+    //make all of the resources unique handles/pointers then they will clean up themselves
+
+    // Window& operator=(Window&& other) = default;
+    // Window(Window&&) = default;
 
     static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
             glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
     }
 
-    GLFWwindow* window = nullptr;
+    struct WindowDeleter {
+        void operator()(GLFWwindow* window) { glfwDestroyWindow(window); }
+    };
+
+    std::unique_ptr<GLFWwindow, WindowDeleter> window;
     vk::UniqueSurfaceKHR us;
 };
 
 int main() {
-    bool enableDebugging = false;
+    bool enableDebugging = true;
     // Window window2;
     vk::ApplicationInfo appInfo("Hello Triangle", VK_MAKE_VERSION(1, 0, 0), "No Engine",
                                 VK_MAKE_VERSION(1, 0, 0), VK_API_VERSION_1_0);
@@ -124,7 +133,14 @@ int main() {
 
     auto instance = vk::createInstanceUnique(instanceCreateInfo);
 
-    Window window{ instance.get() };
+    // using windowPtr = std::shared_ptr<Window>;
+    // std::vector<windowPtr> windowVec;
+    std::vector<Window> windowVec;
+    // windowVec.push_back(std::make_shared<Window>(instance.get()));
+    // windowVec.push_back(std::make_shared<Window>(instance.get()));
+    windowVec.push_back(Window(instance.get()));
+    windowVec.push_back(Window(instance.get()));
+
 
     auto messengerL = [&]() {
         return instance->createDebugUtilsMessengerEXTUnique(
@@ -143,29 +159,38 @@ int main() {
     using callBackType = decltype(messengerL());
     callBackType messenger = enableDebugging ? messengerL() : callBackType{};
 
+
     auto physicalDevices = instance->enumeratePhysicalDevices();
     std::cout << "number of devices  " << physicalDevices.size() << "\n";
 
-    int deviceIndex = std::distance(physicalDevices.begin(),
-                                    std::find_if(physicalDevices.begin(), physicalDevices.end(),
-                                                 [](const vk::PhysicalDevice& physicalDevice) {
-                                                     return strstr(physicalDevice.getProperties().deviceName, "Intel");
-                                                 }));
+    int deviceIndex =
+        std::distance(physicalDevices.begin(),
+                      std::find_if(physicalDevices.begin(), physicalDevices.end(), [](const vk::PhysicalDevice& physicalDevice) {
+                          return strstr(physicalDevice.getProperties().deviceName, "Intel");
+                      }));
     if (deviceIndex == physicalDevices.size()) {
-        std::cout << "No devices found mathing criteria\n";
-        return 1;
+        std::cout << "No devices found mathing criteria, falling back to first found\n";
+        deviceIndex = 0;
     }
     auto physicalDevice = physicalDevices[deviceIndex];
-    std::cout << "using device at device Index " << deviceIndex << " with name "
-              << physicalDevice.getProperties().deviceName << "\n";
+    std::cout << "using device" << physicalDevice.getProperties().deviceName << "at index "
+              << deviceIndex << "\n";
 
     // auto extensionProperties = physicalDevices[0].enumerateDeviceExtensionProperties();
 
     // vk::UniqueSurfaceKHR surface(window.getSurface(instance));
 
-    while (!glfwWindowShouldClose(window.window)) {
+    while (windowVec.size() > 0) {
         // Keep running
+        windowVec.erase(std::remove_if(windowVec.begin(), windowVec.end(),
+                                       [](Window& windowPtr) -> bool {
+                                           return glfwWindowShouldClose(windowPtr.window.get());
+                                       }),
+                        windowVec.end());
+
         // glfwSwapBuffers(window.window);
         glfwPollEvents();
     }
+
+    // vkDestroyInstance(instance.get(), nullptr);
 }
